78ab6b5a3903ddd2f3d6567413dfcab1de64d9d0
Aaditya Sahu <aadityasahu1203@gmail.com>
2026-08-02 12:20:20 +0530

    Time: 1 ms (98.11%), Space: 43.4 MB (36.86%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.java
+++ 3110-score-of-a-string/3110-score-of-a-string.java
@@ -2,8 +2,7 @@ class Solution {
     public int scoreOfString(String s) {
         int ans=0;
         for(int i=0;i<s.length()-1;i++){
-            int d=Math.abs(s.charAt(i)-s.charAt(i+1));
-            ans=ans+d;
+            ans+=Math.abs(s.charAt(i)-s.charAt(i+1));
         }
         return ans;
     }
